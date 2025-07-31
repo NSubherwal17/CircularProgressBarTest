4e22bc5c6e11600162e4b7de5c12f56ed013171b
Naina Subherwal <nainasubherwal@gmail.com>
2025-07-31 10:00:16 -0700

    Initial Commit

---
+++ CircularProgressBarTest/CircularProgressBarTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CircularProgressBarTestApp.swift
+//  CircularProgressBarTest
+//
+//  Created by Scholar on 7/31/25.
+//
+
+import SwiftUI
+
+@main
+struct CircularProgressBarTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
